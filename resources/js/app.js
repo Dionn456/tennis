3057add84f43aaca0dadb810988e743a2a66432b
Jordy <jordy.stevelink2@gmail.com>
2024-04-04 10:53:31 +0200

    install packages

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,11 +3,27 @@ import store from '~/store'
 import router from '~/router'
 import i18n from '~/plugins/i18n'
 import App from '~/components/App'
+import VModal from 'vue-js-modal'
+Vue.use(VModal)
+
+import vSelect from "vue-select";
+Vue.component("v-select", vSelect);
 
 import '~/plugins'
 import '~/components'
 
 Vue.config.productionTip = false
+
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+Vue.prototype.$swal = Swal
+
+import axios from 'axios'
+Vue.prototype.$https = axios
+
+import moment from 'moment'
+import 'moment/dist/locale/nl';
+moment.locale('nl');
+Vue.prototype.moment = moment
 
 /* eslint-disable no-new */
 new Vue({
