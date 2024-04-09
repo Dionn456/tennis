1b4b2dea0babbcddd91a13e6f9650a2695513b37
Jordy <jordy.stevelink2@gmail.com>
2024-04-10 00:00:53 +0200

    personal home page

--- resources/js/pages/welcome.vue
+++ resources/js/pages/welcome.vue
@@ -26,10 +26,10 @@
 
             <div class="container">
               <div class="carousel-caption text-end">
-                <h1>Another example headline.</h1>
-                <p>Some representative placeholder content for the second slide of the carousel.</p>
-                <router-link :to="{ name: 'register' }" class="btn btn-lg btn-primary" active-class="active">
-                  Registreren
+                <h1>Beter worden in tennis?</h1>
+                <p>Log dan in om een les aan te vragen.</p>
+                <router-link :to="{ name: 'login' }" class="btn btn-lg btn-primary" active-class="active">
+                  Login
                 </router-link>
               </div>
             </div>
@@ -44,7 +44,72 @@
           <span class="visually-hidden">Next</span>
         </button>
       </div>
+      <div class="container">
+        <div class="p-3 pb-md-4 mx-auto text-start">
+          <div class="row">
+            <div class="col-md-6">
+              <h1 class="display-4 fw-normal ">Over ons</h1>
 
+              <p class="fs-5 text-muted">
+                Tennishallen Hengelo is een initiatief van Richard van Dongen. Zijn specialiteit ligt in het verhuren van tennisbanen, 
+                waarbij we sportliefhebbers de kans bieden om per uur een baan te reserveren. Ons unieke aanbod: elke tennisbaan is 
+                vernoemd naar een beroemde tennislegende. Zo geniet je niet alleen van onze topfaciliteiten, maar duik je ook in de 
+                rijke historie van de tennissport.</p>
+                <p>Phone: <a href="callto:074 242 2424">074 242 2424</a></p>
+                <p>Email: <a href="mailto:info@tennisbaanhengelo.nl">info@tennisbaanhengelo.nl</a></p>
+                <p>Locatie: <a href="https://maps.google.com/maps?hl=nl&gl=nl&um=1&ie=UTF-8&fb=1&sa=X&ftid=0x47b81196f4533155:0xa8a9026e7f07596e">Veldwijk, Kuipersdijk 32, 7552 BJ Hengelo</a></p>
+
+            </div>
+            <div class="col-md-6">
+              <h1 class="display-4 fw-normal">Openingstijden</h1>
+                <p>Maandag: <span class="text-muted">9:00-23:00</span> uur</p>
+                <p>Dinsdag: <span class="text-muted">9:00-23:00</span> uur</p>
+                <p>Woensdag: <span class="text-muted">9:00-23:00</span> uur</p>
+                <p>Donderdag: <span class="text-muted">9:00-23:00</span> uur</p>
+                <p>Vrijdag: <span class="text-muted">9:00-23:00</span> uur</p>
+                <p>Zaterdag: <span class="text-muted">9:00-23:00</span> uur</p>
+                <p>Zondag: <span class="text-muted">9:00-23:00</span> uur</p>
+              
+            </div>
+          </div>
+        </div>
+
+        <div class="p-3 pb-md-4 mx-auto text-center">
+          <h1 class="display-4 fw-normal">Tennis docenten</h1>
+        </div>
+        <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+        <div class="col">
+          <div class="card mb-4 rounded-3 shadow-sm">
+            <div class="card-header py-3">
+              <h4 class="my-0 fw-normal">Thijmen Meeuwis</h4>
+            </div>
+            <div class="card-body">
+              <img src="/img/Thijmen_meeuwis.png" class="d-block w-100" alt="Wild Landscape"/>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card mb-4 rounded-3 shadow-sm">
+            <div class="card-header py-3">
+              <h4 class="my-0 fw-normal">Mootje teniso</h4>
+            </div>
+            <div class="card-body">
+              <img src="/img/Mootje_teniso.png" class="d-block w-100" alt="Wild Landscape"/>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card mb-4 rounded-3 shadow-sm">
+            <div class="card-header py-3">
+              <h4 class="my-0 fw-normal">Serena Slagmisser</h4>
+            </div>
+            <div class="card-body">
+              <img src="/img/Serena_slagmisser.png" class="d-block w-100" alt="Wild Landscape"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
      <!-- -->
     </div>
   </div>
